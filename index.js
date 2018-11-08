bfbd59304d77a060907d26fa375386b7b9f8f6e9
unknown <aviso_daniel@yahoo.com>
2018-11-08 12:12:53 +0800

    Made a server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3400;
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'server/views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index.pug');
+});
+
+//app.use('/', index);
+
+app.listen(PORT, () => {
+    console.log(`Server is running under port ${PORT}`);
+});
